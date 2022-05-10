2b345bd3ef2d0ee352ff549cd0014f3e7f917b4c
helielcust <helieldamaceno@gmail.com>
2022-05-10 16:01:02 -0400

    commit 10/05 16:00

--- AppTodo/src/app/home/home.page.ts
+++ AppTodo/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,14 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tarefas: any[]=[];
 
-  constructor(private alertCrtl: AlertController) {}
+  constructor(private alertCrtl: AlertController, private toastCtrl: ToastController) {
+    let tarefaSalva = localStorage.getItem('tarefaUsuario');
+    if(tarefaSalva !=null){
+      this.tarefas = JSON.parse(tarefaSalva);
+    }
+  }
 
   async showAdd(){
     const alert =await this.alertCrtl.create({
@@ -33,15 +40,35 @@ export class HomePage {
         },
         {
           text: 'Adicionar',
-          handler: () => {
-            console.log('Adicionado com sucesso!');
+          handler: (form) => {
+            this.adicionaTarefa(form.tarefa1);
           },
         },
       ],
     });
 
     await alert.present();
+  
+  }
 
+  async adicionaTarefa(novaTarefa:string){
+    if(novaTarefa.trim().length < 1){
+      const toast=await this.toastCtrl.create({
+        message: 'Por favor, Digite a tarefa!',
+        duration:2000,
+        position:'top',
+      })
+      toast.present();
+      return;
+    }
+
+    const tarefa = { nome:novaTarefa, realizada:false};
+  this.tarefas.push(tarefa);
+  this.salvaLocalStorage();
+  }
+
+  salvaLocalStorage(){
+    localStorage.setItem('tarefaUsuario', JSON.stringify(this.tarefas));
   }
 
 }
